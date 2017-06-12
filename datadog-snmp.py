ee44fefa376d92022eed476acbd3a674e1066791
James Fleming <james@electronic-quill.net>
2017-06-12 21:56:42 +0200

    Add a configurable limit to the number of processes

--- datadog-snmp.py
+++ datadog-snmp.py
@@ -45,6 +45,9 @@ DEFAULT_CONFIGPATH='config.json'
 # How many seconds to pause between runs
 PERIOD_DEFAULT=60
 
+# Maximum number of processes to run at one time
+MAXPROCS_DEFAULT=2
+
 
 # The actual code
 
@@ -73,7 +76,7 @@ def main(logger, configpath):
     - handle the multiprocessing overhead
     '''
     # Process-management
-    procs=[]            # A list of running processes
+    procs={}            # A dict of running processes
     mgr=mp.Manager()    # Source of shared-state structures
     state=mgr.dict()    # The shared dict we'll use for sharing state
     queue=mp.Queue()    # For passing results from SNMP queries to the Datadog writer
@@ -83,11 +86,15 @@ def main(logger, configpath):
     if 'period' in configs['global']:
         period=configs['global']['period']
     else:
-        period=PERIOD_GLOBAL
+        period=PERIOD_DEFAULT
+    if 'max_procs' in configs['global']:
+        max_procs=configs['global']['max_procs']
+    else:
+        max_procs=MAXPROCS_DEFAULT
     # Start the process that reads the queue and feeds Datadog
     writer=mp.Process(target=result_writer.run, args=(queue, configs['global']['datadog_api_key'], logger), name='writer')
     writer.start()
-    # Periodically poll the targets
+    # Periodically poll the targets until forcibly interrupted
     while True:
         starttime=int(time.time())
         logger.info('Starting run with timestamp %d' % starttime)
@@ -98,18 +105,56 @@ def main(logger, configpath):
             logger.info("Config file's timestamp has changed from %s to %s; re-reading." % (config_mtime, config_curr_mtime))
             configs=read_configs(configpath)
             config_mtime=int(os.path.getmtime(configpath))
-        # Kick off the SNMP-querying processes
-        for target in configs['metrics']:
-            proc=mp.Process(target=snmp_query.query_device,
-                    args=(target, logger, state, period, queue),
-                    name=target['hostname'])
-            procs.append(proc) # Add the process to the list before starting it
-            proc.start()
-        # Gather the processes back in
-        for proc in procs:
-            proc.join()
-        # Prove we got something in the state dict
-        logger.debug('State: %s' % state)
+        # Work through the list of targets
+        target_ctr=0
+        target_total = len(configs['metrics'])   # The number of targets to query
+        unfinished=True # Do we still have targets to query?
+        while unfinished:
+            # Harvest any processes that have finished their work
+            hostnames=list(procs.keys())
+            for hostname in hostnames:
+                if not procs[hostname].is_alive():
+                    logger.debug('Finished querying %s' % hostname)
+                    procs[hostname].join()
+                    # Remember to remove it from the dict
+                    del procs[hostname]
+            # If we're already running the max allowed processes, pause a second then go around again
+            if len(procs) >= max_procs:
+                time.sleep(1)
+            # We're clear to add another one
+            else:
+                hostname=configs['metrics'][target_ctr]['hostname']
+                logger.debug('Starting to query %s' % hostname)
+                proc=mp.Process(target=snmp_query.query_device,
+                    args=(configs['metrics'][target_ctr], logger, state, period, queue),
+                    name=hostname)
+                procs[hostname]=proc # Add the process to the control dict before starting it
+                proc.start()
+                # Move on to the next target
+                target_ctr=target_ctr + 1
+                # Have we reached the end of the list?
+                if target_ctr == target_total:
+                    unfinished=False
+        # Now they've all been dispatched, harvest them as they complete
+        logger.debug('All targets are being queried')
+        running=True
+        while running:
+            # Check each of the processes
+            hostnames=list(procs.keys())
+            for hostname in hostnames:
+                if not procs[hostname].is_alive():
+                    logger.debug('Finished querying %s' % hostname)
+                    procs[hostname].join()
+                    # Remember to remove it from the dict
+                    del procs[hostname]
+            # Still some left
+            if len(procs) > 0:
+                # Pause a second, instead of hammering the CPU with this
+                time.sleep(1)
+            # All done; exit the loop
+            else:
+                running=False
+        logger.debug('Harvested all worker processes; finished querying all targets')
         # Pause until the next run
         # being reasonably sure to start _on_ the minute (or whatever)
         endtime=int(time.time())
@@ -126,7 +171,6 @@ def main(logger, configpath):
 
 
 # When invoked as a script, rather than a library
-
 if __name__ == '__main__':
     # Set up logging to STDERR
     mp.log_to_stderr()
